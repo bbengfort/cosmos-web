6213323f14e357cb696fb69eea54d08004e71507
Benjamin Bengfort <benjamin@bengfort.com>
2023-12-02 17:01:42 -0600

    sticky footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,8 +30,9 @@ const Footer = () => {
   }, [])
 
   return (
-    <div className='container'>
-      <footer className='d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top'>
+    <footer className='footer'>
+      <div className='container'>
+        <div className='d-flex flex-wrap justify-content-between align-items-center py-3 my-3 border-top'>
         <p className='col-md-4 mb-0 text-muted'>Made with &spades; by <a className='link-dark text-decoration-none' href="https://github.com/bbengfort">@bbengfort</a></p>
         <div className='col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto'>
           <img src="/logo192.png" height="32" width="32" />
@@ -39,8 +40,9 @@ const Footer = () => {
         <p className='col-md-4 text-end text-muted mb-0'>
           Cosmos API <span id="cosmos-api-version">v{apiStatus.version}</span>
         </p>
-      </footer>
-    </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import { AuthProvider } from './context/AuthProvider';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
